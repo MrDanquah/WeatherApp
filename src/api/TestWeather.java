ae71fe4069c283a66e01dce47c647100571c8126
Kdanquah <okdanquah@gmail.com>
2016-02-12 16:29:48 +0000

    Weather App API

---
+++ src/api/TestWeather.java
@@ -0,0 +1,16 @@
+package api;
+
+public class TestWeather
+{
+
+	public static void main(String[] args)
+	{
+		WeatherAPI weather = new WeatherAPI("2368529");
+		//System.out.println(weather.theWeatherRSS);
+		for(int i = 0; i < weather.weatherForecastList.size(); i++)
+		{
+			System.out.println("Low temperature: " + weather.weatherForecastList.get(i).lowTemp + " High Temperature: " +
+			weather.weatherForecastList.get(i).highTemp + " " + weather.weatherForecastList.get(i).dateTemp + " " + weather.weatherForecastList.get(i).textTemp);
+		}
+	}
+}
